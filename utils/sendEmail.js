ca607e5b55b6dd4b44f77966706a00666daea886
Krish <krishkrm07@gmail.com>
2026-06-28 21:18:20 +0530

    fix sendEmail() 2

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,31 +1,24 @@
 const nodemailer = require("nodemailer");
 
-await transporter.verify();
-console.log("SMTP connection successful");
 const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    console.log("Sending email to:", to);
+  // Verify SMTP connection
+  await transporter.verify();
+  console.log("SMTP connection verified.");
 
-  const info = await transporter.sendMail({
+  await transporter.sendMail({
     from: `"PayPing" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     text,
   });
-
-  console.log("Email sent:", info.messageId);
 };
 
 module.exports = sendEmail;
