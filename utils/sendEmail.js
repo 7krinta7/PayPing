ff8952640c92e69ebeb31249cc961de45fbd4267
Krish <krishkrm07@gmail.com>
2026-06-28 21:14:23 +0530

    fix sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,13 +1,20 @@
 const nodemailer = require("nodemailer");
 
+await transporter.verify();
+console.log("SMTP connection successful");
 const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
 
     console.log("Sending email to:", to);
 
